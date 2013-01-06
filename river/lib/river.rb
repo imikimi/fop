5d73c252bccca396b728ae9fdad58fa1629ca621
Shane Brinkman-Davis <shanebdavis@gmail.com>
2013-01-06 15:40:19 -0800

    simplified bb required

--- river/lib/river.rb
+++ river/lib/river.rb
@@ -1,13 +1,6 @@
 bb_dev_path = File.expand_path(File.join(File.dirname(__FILE__),"..","..","..","development","babel_bridge","lib","babel_bridge.rb"))
-puts "#{bb_dev_path} exists? #{File.exists? bb_dev_path}"
-if File.exists?(bb_dev_path)
-  puts "Using development version of Babel-Bridge: #{bb_dev_path}"
-  require bb_dev_path
-else
-  require "babel_bridge"
-end
-puts "Babel-Bridge Version: #{BabelBridge::VERSION}"
 
+require "babel_bridge"
 require "json"
 
 %w{
