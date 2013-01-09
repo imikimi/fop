db9b77bec78f1366b8d14982fbafbaa6dec5deb3
Shane Brinkman-Davis <shanebdavis@gmail.com>
2013-01-08 16:10:47 -0800

    more river_tool updates

--- river/bin/river_tool.rb
+++ river/bin/river_tool.rb
@@ -3,12 +3,26 @@ require File.expand_path(File.join(File.dirname(__FILE__),"../lib/river.rb"))
 require "trollop"
 
 def river(args)
+  v = "River runtime v#{River::VERSION}"
+
   options = Trollop::options(args) do
+    banner <<ENDBANNER
+#{v}
+
+Usage:
+
+  #{__FILE__} [options] <files>
+
+Options:
+
+ENDBANNER
+
     opt :transcode, "Output the text source-model"
     opt :json, "Output the json source-model"
   end
 
   files = args
+  Trollop::die "must specify at least one file" if files.length == 0
 
   parser = River::Parser.new
   runtime = River::Runtime::Stack.new
